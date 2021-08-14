586dc0aad656c7caa4d4345c2f1b5ee8256d8afb
catrincm <catrin.cmoore@gmail.com>
2021-08-14 15:58:14 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
@@ -25,3 +25,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+cd ..
